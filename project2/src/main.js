ea7ac92412ea5b123e1d0f86e073cab316c9d831
KeCheng Yu <kxy3643@rit.edu>
2020-10-23 23:45:57 -0400

    Bug fixes, DJ mode started

--- project2/src/main.js
+++ project2/src/main.js
@@ -10,6 +10,7 @@ let drawParams = {
   showInvert : false,
 };
 
+const gui = new dat.GUI({ width: 400 });
 
 const DEFAULTS = Object.freeze({
 	sound1  :  "media/New Adventure Theme.mp3"
@@ -19,7 +20,8 @@ function init(){
 	console.log("init called");
   console.log(`Testing utils.getRandomColor() import: ${utils.getRandomColor()}`);
   audio.setupWebaudio(DEFAULTS.sound1);
-	let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
+  let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
+  setupGUI();
   setupUI();
   canvas.setupCanvas(canvasElement,audio.analyserNode);
   
@@ -40,6 +42,7 @@ const controllerObj = {
   _synthLowPass : false,
   _lowPassFreq: 100,
   _lowPassQ : 0.5,
+  _djMode : false,
 
   get showBars(){return this._showBars;},
   set showBars(value){this._showBars = value; drawParams.showBars = value;},
@@ -68,56 +71,38 @@ const controllerObj = {
   get lowPassQ(){return this._lowPassQ;},
   set lowPassQ(value){this._lowPassQ = value; audio.setLowPassQ(value)},
 
-  get HighPassFreq(){return this._highPassFreq;},
-  set HighPassFreq(value){this._highPassFreq = value; audio.setHighPassFreq(value)},
+  get highPassFreq(){return this._highPassFreq;},
+  set highPassFreq(value){this._highPassFreq = value; audio.setHighPassFreq(value)},
 
-  get HighPassQ(){return this._highPassQ;},
-  set HighPassQ(value){this._highPassQ = value; audio.setHighPassQ(value)},
+  get highPassQ(){return this._highPassQ;},
+  set highPassQ(value){this._highPassQ = value; audio.setHighPassQ(value)},
 
   get volume(){return this._volume;},
   set volume(value){
     this._volume = value;
     audio.setVolume(value/100);
-    volumeLabel.innerHTML = Math.round((value/100));
-  }
-}
+  },
+
+  get djMode(){return this._djMode;},
+  set djMode(value){this._djMode = value;}
+};
+
+let canvasFolder, visFolder, maniFolder, hpfolder, lowfolder, hpcheck, lowcheck, 
+hfreq,hq,lfreq,lq, djCheck, volumeSlider;
 
 function setupUI(){
-  const gui = new dat.GUI({ width: 400 });
+  
   //gui.close();
 
-  const playButton = document.querySelector("#playButton")
+  const playButton = document.querySelector("#playButton");
   let trackSelect = document.querySelector("#trackSelect");
-
-  let canvasFolder = gui.addFolder('Visualizer Options');
-  let visFolder = canvasFolder.addFolder('Visualization');
-  let maniFolder = canvasFolder.addFolder('Manipulation');
-  visFolder.add(controllerObj, 'showBars').name('Show Bars');
-  visFolder.add(controllerObj, 'showGradient').name('Show Gradient');
-  visFolder.add(controllerObj, 'showCircles').name('Show Circles');
-  maniFolder.add(controllerObj, 'showNoise').name('Show Noise');
-  maniFolder.add(controllerObj, 'showInvert').name('Show Invert');
-
-  let hpfolder = gui.addFolder('High Pass Filter');
-  hpfolder.add(controllerObj, 'synthHighPass').name('High Pass Filter');
-  hpfolder.add(controllerObj, 'HighPassFreq').min(1).max(20000).step(1).name('Freq');
-  hpfolder.add(controllerObj, 'HighPassQ').min(0.5).max(24.5).step(1).name('Q');
-
-  let lowfolder = gui.addFolder('Low Pass Filter');
-  lowfolder.add(controllerObj, 'synthLowPass').name('Low Pass Filter');
-  lowfolder.add(controllerObj, 'lowPassFreq').min(1).max(20000).step(1).name('Freq');
-  lowfolder.add(controllerObj, 'lowPassQ').min(0.5).max(24.5).step(1).name('Q')
-
-  gui.add(controllerObj, 'volume').min(0).max(100).step(1).name('Volume');
-
-  // add .onclick event to button
   
   playButton.onclick = e => {
     console.log(`audioCtx.state before = ${audio.audioCtx.state}`);
 
     if(audio.audioCtx.state == "suspended"){
       audio.audioCtx.resume();
-    }
+    };
 
     console.log(`audioCtx.state after = ${audio.audioCtx.state}`);
     if(e.target.dataset.playing == "no"){
@@ -127,17 +112,97 @@ function setupUI(){
     else{
       audio.pauseCurrentSound();
       e.target.dataset.playing = "no";
-    }
-  }
-
+    };
+  };
 
   trackSelect.onchange = e => {
     audio.loadSoundFile(e.target.value);
     if(playButton.dataset.playing = "yes"){
       playButton.dispatchEvent(new MouseEvent("click"));
-    }
+    };
   };
+
 } // end setupUI
+
+function setupGUI()
+{
+  canvasFolder = gui.addFolder('Visualizer Options');
+  visFolder = canvasFolder.addFolder('Visualization');
+  maniFolder = canvasFolder.addFolder('Manipulation');
+
+  visFolder.add(controllerObj, 'showBars').name('Show Bars');
+  visFolder.add(controllerObj, 'showGradient').name('Show Gradient');
+  visFolder.add(controllerObj, 'showCircles').name('Show Circles');
+  maniFolder.add(controllerObj, 'showNoise').name('Show Noise');
+  maniFolder.add(controllerObj, 'showInvert').name('Show Invert');
+
+  hpfolder = gui.addFolder('High Pass Filter');
+  hpcheck = hpfolder.add(controllerObj, 'synthHighPass').name('High Pass Filter');
+  
+  hpcheck.onFinishChange(function(){
+    if(controllerObj.synthHighPass)
+    {
+      hfreq = hpfolder.add(controllerObj, 'highPassFreq').min(1).max(20000).step(1).name('Freq');
+      hq = hpfolder.add(controllerObj, 'highPassQ').min(0.5).max(24.5).step(0.5).name('Q');
+      
+    }
+    else
+    {
+      hfreq.remove();
+      hq.remove();
+    };
+  });
+
+  lowfolder = gui.addFolder('Low Pass Filter');
+  lowcheck =lowfolder.add(controllerObj, 'synthLowPass').name('Low Pass Filter');
+
+  lowcheck.onFinishChange(function(){
+    if(controllerObj.synthLowPass)
+    {
+      lfreq = lowfolder.add(controllerObj, 'lowPassFreq').min(1).max(20000).step(1).name('Freq');
+      lq = lowfolder.add(controllerObj, 'lowPassQ').min(0.5).max(24.5).step(0.5).name('Q');
+    }
+    else
+    {
+      lfreq.remove();
+      lq.remove();
+    };
+  });
+  
+
+  volumeSlider = gui.add(controllerObj, 'volume').min(0).max(100).step(1).name('Volume');
+
+  djCheck = gui.add(controllerObj, 'djMode').name("DJ MODE");
+
+  djCheck.onChange(function(){
+    if(controllerObj.djMode)
+    {
+      gui.removeFolder(canvasFolder);
+      gui.removeFolder(hpfolder);
+      gui.removeFolder(lowfolder);
+      volumeSlider.remove();
+      djModeToggle();
+    }
+    else
+    {
+      djCheck.remove();
+      djModeToggle();
+      setupGUI();
+    }
+  });
+}
+
+function djModeToggle()
+{
+  if(controllerObj.djMode)
+  {
+    document.querySelector("body").style.backgroundColor = '#2e2e2e';
+  }
+  else
+  {
+    document.querySelector("body").style.backgroundColor = '#eeeeee';
+  }
+}
 
 function loop(){
     requestAnimationFrame(loop);
